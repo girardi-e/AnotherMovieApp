caba7882034689b1a9a28e2f9a9be073bfdd6561
Emilio Girardi <emilio.g.girardi@gmail.com>
2021-09-23 19:11:50 +0200

    changed the url for poster_url

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'json'
 require 'open-uri'
 
+Movie.destroy_all
+
 url = 'https://tmdb.lewagon.com/movie/now_playing'
 
 movies = JSON.parse(URI.open(url).read)['results']
@@ -8,7 +10,7 @@ movies = JSON.parse(URI.open(url).read)['results']
 movies.each do |movie|
   title = movie['original_title']
   overview = movie['overview']
-  poster_url = "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}"
+  poster_url = "https://image.tmdb.org/t/p/w500#{movie['poster_path']}"
   rating = movie['vote_average']
   Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
 end
