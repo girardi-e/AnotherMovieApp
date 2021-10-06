1ad669f19ee4bfcc9a6785e4f9d7e07e74aa4124
Emilio Girardi <emilio.g.girardi@gmail.com>
2021-10-06 15:50:29 +0200

    added select2 to movie list

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
+import 'select2/dist/css/select2.min.css'
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
+import { initSelect2 } from './plugins/init_select2';
 import { navSlide } from '../components/navbar';
 import { bannerLoop} from '../components/banner';
 
@@ -20,4 +22,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
   navSlide();
   bannerLoop();
+  initSelect2();
 });
