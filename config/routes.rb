b8b9f3051e75dab4522bead5b3971ff0c1d89424
Emilio Girardi <emilio.g.girardi@gmail.com>
2021-10-06 13:02:17 +0200

    added movie cards to list show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  get 'lists/new', to: 'lists#new', as: :new_list
-  post 'lists', to: 'lists#create'
-  get 'lists/:id', to: 'lists#show', as: :list
-  get 'bookmarks/new', to: 'bookmarks#new', as: :new_bookmark
-  post 'bookmarks', to: 'bookmarks#create'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
